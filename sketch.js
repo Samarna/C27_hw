d4d1d8cc065974f68304151e49cfff77f0f25361
Samarna <63645732+Samarna@users.noreply.github.com>
2020-05-20 16:13:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,9 +15,9 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
 
-    bird = new Bird(100,100);
+    bob = new Bob(100,100);
     
-    sling = new Sling(bird.body,{x : 200,y : 100});
+    string = new String(bird.body,{x : 200,y : 100});
 }
 
 function draw(){
@@ -29,13 +29,18 @@ function draw(){
     fill(225,225,225);
     ground.display();
 
-    bird.display();
-    sling.display();    
+    bob.display();
+    string.display();    
 }
 function keyPressed(){
-    if(keyCode === 32 && bird.body.speed < 1){
-        bird.trajectory = [];
-        Matter.Body.setPosition(bird.body,{x : 200 , y :50})
-        slingshot.attach(bird.body);
+    if(keyCode === 32 && bob.body.speed < 1){
+        bob.sprite.x = mouseX
+        bob.sprite.y = mouseY
+    }
+}
+function stop(){
+    if(keyCode === 13 && bob.body.speed > 1){
+        bob.x = 100;
+        bob.y = 100;
     }
 }
